eddd0f85a9969f60112a8e0348b4bb3ff4977c27
Thomas <quentin.sebring@icloud.com>
2021-04-13 11:03:54 -0500

    Finished Menu Functionality

--- Assets/Attributions.cs
+++ Assets/Attributions.cs
@@ -2,3 +2,4 @@
 //
 //
 //Background image from https://www.nasa.gov/mission_pages/hubble/multimedia/index.html
+//Volume image from https://www.shutterstock.com/image-vector/volume-icon-209142427
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,33 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
     List<Gravity> physObjects;
 
+    public GameObject startButton, creditsButton, howToButton, volumeButton, backButton;
+    public GameObject titleText, creditsText, howToText;
+    public GameObject volumeSlider;
+    public GameObject canvas;
+    public GameObject events;
+    public GameObject background;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(events);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Destroy(canvas);
+            Destroy(events);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Instance = this;
         physObjects = new List<Gravity>();
         
         foreach (Gravity g in FindObjectsOfType<Gravity>()) {
@@ -38,5 +61,52 @@ public class GameManager : MonoBehaviour
         }
 
         return G * c;
+    }
+
+    public void startOnClick()
+    {
+
+    }
+
+    public void creditsOnClick()
+    {
+        titleText.SetActive(false);
+        startButton.SetActive(false);
+        howToButton.SetActive(false);
+        creditsButton.SetActive(false);
+        backButton.SetActive(true);
+        creditsText.SetActive(true);
+    }
+
+    public void howToOnClick()
+    {
+        titleText.SetActive(false);
+        startButton.SetActive(false);
+        howToButton.SetActive(false);
+        creditsButton.SetActive(false);
+        backButton.SetActive(true);
+        howToText.SetActive(true);
+    }
+
+    public void volumeOnClick()
+    {
+        if (volumeSlider.activeSelf == true)
+        {
+            volumeSlider.SetActive(false);
+        } else
+        {
+            volumeSlider.SetActive(true);
+        }
+    }
+
+    public void backOnClick()
+    {
+        titleText.SetActive(true);
+        startButton.SetActive(true);
+        howToButton.SetActive(true);
+        creditsButton.SetActive(true);
+        backButton.SetActive(false);
+        creditsText.SetActive(false);
+        howToText.SetActive(false);
     }
 }
